799e75c1cf07de29ca5533856e9de670912fdfe1
Roman <roman.lossin@icloud.com>
2018-06-08 11:00:19 +0200

    Added event model

---
+++ src/Model/EventModelTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Spqr\Eventlist\Model;
+
+use Pagekit\Database\ORM\ModelTrait;
+
+/**
+ * Class EventModelTrait
+ *
+ * @package Spqr\Eventlist\Model
+ */
+trait EventModelTrait
+{
+    use ModelTrait;
+    
+    /**
+     * @Saving
+     */
+    public static function saving($app_event, Event $event)
+    {
+        $event->modified = new \DateTime();
+        $i               = 2;
+        $id              = $event->id;
+        while (self::where('slug = ?', [$event->slug])->where(function (
+            $query
+        ) use ($id) {
+            if ($id) {
+                $query->where('id <> ?', [$id]);
+            }
+        })->first()) {
+            $event->slug = preg_replace('/-\d+$/', '', $event->slug).'-'.$i++;
+        }
+    }
+    
+}
